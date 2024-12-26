31a69b44d1824f7de699815b14a93d6ac802ab5e
erica-lucia <ericacarreonbenites@gmail.com>
2024-12-26 23:59:05 +0100

    Completando el scripts LevelManager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -3,16 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
-{
+{ 
+    public static LevelManager sharedInstance;
+    public List<LevelBlock>alltheLevelBlocks=new List<LevelBlock>();
+    public List<LevelBlock>currentLevelBlocks=new List<LevelBlock>();
+    public Transform LevelStartPosition;
+
+
+    private void Awake(){ 
+        if(sharedInstance==null){ 
+            sharedInstance= this;
+        }
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
+    {
+        GenerateInitialBlocks();
+    }
+
+    // Update is called once per frame
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    //Metodo para añadir un bloque
+    public void AddLevelBlock(){ 
+
+    } 
+
+    public void RemoveLevelBlock(){ 
+
+    } 
+
+    public void RemoveAllLevelBlocks(){ 
+
+    } 
+
+    //metodo para generar los bloques iniciales 
+    public void GenerateInitialBlocks(){ 
+        for (int i =0; i<2; i++){ 
+            AddLevelBlock();
+        }
     }
 }
